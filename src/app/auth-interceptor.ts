284a7e9231028cae51c22dcbe61875ac3f2f2ac0
Manasa-Kura <manasa.kura@ahex.co.in>
2026-04-08 15:12:39 +0530

    Implemented Authorization Header(token)

--- src/app/auth-interceptor.ts
+++ src/app/auth-interceptor.ts
@@ -1,17 +1,29 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { tap} from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log("Interceptor Request:", req);
-  return next(req).pipe(    //next(req)=>sends req to api,.pipe() handles response+error
-    tap((res)=>{
+  // GET TOKEN
+  const token = localStorage.getItem('token');
+  let modifiedReq = req;
+  // ADD TOKEN
+  if (token) {
+    modifiedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+  console.log("Interceptor Request:", modifiedReq);
+  return next(modifiedReq).pipe(    //next(req)=>sends req to api,.pipe() handles response+error
+    // RESPONSE HANDLING
+    tap((res) => {
       console.log("Interceptor Response:", res);
     }),
-    catchError((error)=>{
-      console.log("Interceptor Error:",error);
-      alert("something went wrong");
-      return throwError(()=>error);
+    //ERROR HANDLING
+    catchError((error) => {
+      console.log("Interceptor Error:", error);
+      alert("Something went wrong!");
+      return throwError(() => error);
     })
   );
 };
