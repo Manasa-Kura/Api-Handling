8f6da6b492b73042146dd79eeabed2db55c739be
Manasa-Kura <manasa.kura@ahex.co.in>
2026-04-08 12:27:00 +0530

    Enhanced Angular interceptor with global response logging and error handling

--- src/app/auth-interceptor.ts
+++ src/app/auth-interceptor.ts
@@ -1,8 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-
+import { tap} from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-
   console.log("Interceptor Request:", req);
-
-  return next(req);
+  return next(req).pipe(    //next(req)=>sends req to api,.pipe() handles response+error
+    tap((res)=>{
+      console.log("Interceptor Response:", res);
+    }),
+    catchError((error)=>{
+      console.log("Interceptor Error:",error);
+      alert("something went wrong");
+      return throwError(()=>error);
+    })
+  );
 };
